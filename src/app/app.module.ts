b42a526f01378584ed5d0fde34a0d7c2ae860642
Agustín Bosio <bosio.agustinariel@gmail.com>
2022-09-17 00:54:28 -0300

    desafio 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeftMenuComponent } from './components/left-menu/left-menu.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { InfoCardsComponent } from './components/info-cards/info-cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftMenuComponent,
+    ToolbarComponent,
+    InfoCardsComponent
   ],
   imports: [
     BrowserModule,
